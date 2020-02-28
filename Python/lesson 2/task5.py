104a1f7372118dca1b82486d83cd80ddb3d70ae7
Ildar Yakhin <Ildar.Yakhin@gmail.com>
2020-02-28 20:03:33 +0300

    Task 5 done

--- Python/lesson 2/task5.py
+++ Python/lesson 2/task5.py
@@ -6,22 +6,18 @@
 Пользователь ввел число 8. Результат: 8, 7, 5, 3, 3, 2.
 Пользователь ввел число 1. Результат: 7, 5, 3, 3, 2, 1.
 Набор натуральных чисел можно задать непосредственно в коде, например, my_list = [7, 5, 3, 3, 2].'''
-def rating(x):
-    pass
-a = [7, 5, 3, 3, 2, 1]
-a.sort(reverse = True)
-print(a)
-# x = int(input())
-# print(rating(x))
 
-# if __name__ == '__main__':
-#     print("Example:")
-#     print(rating([4, 6, 2, 2, 6, 4, 4, 4]))
-#
-#     # These "asserts" are used for self-checking and not for an auto-testing
-#     assert list(rating([4, 6, 2, 2, 6, 4, 4, 4])) == [4, 4, 4, 4, 6, 6, 2, 2]
-#     assert list(rating(['bob', 'bob', 'carl', 'alex', 'bob'])) == ['bob', 'bob', 'bob', 'carl', 'alex']
-#     assert list(rating([17, 99, 42])) == [17, 99, 42]
-#     assert list(rating([])) == []
-#     assert list(rating([1])) == [1]
-#     print("Coding complete!")
+a_list = [7, 3, 3, 5, 2, 1]
+def rating(x):
+    a_list.sort(reverse=True)
+    if x == a_list[-1]:
+        a_list.append(x)
+        return a_list
+    for i in range(len(a_list)):
+        if x > a_list[i]:
+            a_list.insert(i, x)
+            return a_list
+
+x = int(input('Give me new element \n'))
+print(rating(x))
+
